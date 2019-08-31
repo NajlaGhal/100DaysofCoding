4ffa57af947a800cd3adca46e98f820104a7065d
NajlaGhal <36755160+NajlaGhal@users.noreply.github.com>
2019-08-31 08:14:58 +0300

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,44 @@
+#This is the first code
+print("Hello World")
+if 5 > 1 :
+   print("Five is greater than one")
+# print("Good morning)
+# This is the nultiline comment
+""" To write more than
+one line comment
+we can use triple quot"""
+#This part for day3 Creating Variables
+x , y, z = "Apple", 3, 4
+print(x)
+print(y)
+print(z)
+a = b = "Same value for 2 variables"
+print (a)
+print(b)
+print("vraible x is:" + x)
+x =  "python is"
+y = "Great"
+z= x+y
+print(z)
+a = 55
+b = 145
+c = a+b
+print (c)
+#This part for day4 Python Numbers
+a = 23456 # int
+b = 2.34  # float
+c = 354j  # complex
+print(type(a))
+print(type(c))
+x = 34e3
+print(type(x))
+# Convert int to float
+x = 55
+y = float(x)
+print (y)
+print(type(y))
+# this part for random Number
+import random
+print(random.randrange(1,10))
+
+
